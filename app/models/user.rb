e94e26a3e59d316d71a726b4bbbec91d388bd331
Chester Syron <csyron.damian14@gmail.com>
2018-09-19 14:18:16 +0800

    All functions are working

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,8 +29,8 @@ class User < ApplicationRecord
   end
 
   def self.search(param)
-    param.strip
-    param.downcase
+    param.strip!
+    param.downcase!
     to_send_back = (first_name_matches(param) + last_name_matches(param) + email_matches(param)).uniq
     return nil unless to_send_back
     to_send_back
